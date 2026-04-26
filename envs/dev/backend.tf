e273513fb82274a25c35e3aeb83adcaed4e98fcf
viraj1004 <lukkaviraj@gmail.com>
2026-04-26 09:47:50 +0000

    added viraj2026-04-26

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-284934320341"
+    bucket       = zen-pharma-terraform-state-${ACCOUNT_ID}
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
